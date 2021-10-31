d2c15988c6f3bb9159c29a73c4ff74d7b0805e6c
Carinaa113 <carina.padurariu@stud.ubbcluj.ro>
2021-10-31 20:14:03 +0200

    tema lab 567-cu modificare in lista

--- Logic/functionalitate.py
+++ Logic/functionalitate.py
@@ -1,46 +1,53 @@
 #reducerea pretului in functie de discountul aplicat
-def reducere(lista):
-    listaNoua=[]
-    for carte in lista :
-        if getTip_Reducere(carte)=="silver":
-            cartenoua=creeazaCarte(
-                getId(carte),
-                getTitlu(carte),
-                getGen(carte),
-                getPret(carte)*0,95,
-                getTip_Reducere(carte)
-            )
-            listaNoua.append(cartenoua)
-        elif getTip_Reducere(carte)=="gold":
-             cartenoua=creeazaCarte(
-                 getId(carte),
-                 getTitlu(carte),
-                 getGen(carte),
-                 getPret(carte)*0.90,
-                 getTip_Reducere(carte)
-             )
-            listaNoua.append(cartenoua)
-        else
-            listaNoua.append(carte)
-        return listaNoua
 
-#Modificarea genului pentru un titlu dat.
+def AplicareDiscount(lista):
+    '''
+    Aplicam un discount de 5% pentru reduceri silver si 10% pentru reduceri gold
+    :param lista: lista de carti
+    :return: noua lista de carti,modificata
+    '''
 
-def modificGenul(gennou,titlu,lista):
-    lnoua=[]
+    listanoua=[]
     for carte in lista:
-        if titlu==getTitlu(lista):
-           listanoua=creeazaCarte(
-                 getId(carte),
-                 getTitlu(carte),
-                 getGen(carte),
-                 getPret(carte),
-                 getTip_Reducere(carte)
-             )
-           lnoua.append(listanoua)
-        else:
-           lnoua.append(carte)
-    return lnoua
+        if getTip_Reducere(carte)=="Silver":
+            carteNoua=creeazaCarte(
+            getId(carte),
+            getTitlu(carte),
+            getGen(carte),
+            getPret(carte)*19/20,
+            getTip_Reducere(carte)
+        )
 
-# Ordonarea vânzărilor crescător după preț.
+          listanoua.append(carteNoua)
+    elif getTip_Reducere(carte)=="Gold":
+         carteNoua=creeazaCarte(
+             getId(carte),
+             getTitlu(carte),
+             getGen(carte),
+             getPret(carte)*9/10,
+             getTip_Reducere(carte)
+         )
+         listanoua.append(carteNoua)
+    else:
+         listanoua.append(carte)
 
+    return listanoua
+
+#redarea pretului nou in functie de gen
+
+def pretminim(lista):
+    '''
+    Dterminam pretul minim pentru fiecare gen
+    :param lista: lista cartilor
+    :return: pretul minim in functie de gen
+    '''
+    rezultat={}
+    for carte in lista:
+        gen=getGen(carte)
+        pret=getPret(carte)
+        if gen in rezultat:
+            if pret<rezultat[gen]:
+                rezultat[gen]=pret
+            else:
+                rezultat[gen]
+    return rezultat
