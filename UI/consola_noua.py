e045504094b9c36d7a8b8053e5d5c907cfe8792e
Carinaa113 <carina.padurariu@stud.ubbcluj.ro>
2021-11-01 20:09:27 +0200

    tema lab 567-cu modificare in lista

--- UI/consola_noua.py
+++ UI/consola_noua.py
@@ -1,29 +1,46 @@
-from Logic.CRUD import *
+from UI.console import showAll
+from Logic.CRUD import stergeCarte,modificaCarte,adaugaCarte
+from Domain.carte import toString
 
 
-def showAll(lista):
+def ShowAll(lista):
     for carte in lista:
         print(toString(carte))
 
 
-def linia_principala(lista):
+def consola_noua(lista):
     while True:
-        givenString=input()
-        if givenString=="help"
-            print("add,id,nume,descriere,pret,tip_reducere")
-            print("delete,id")
-            print("showALL")
-            print("Exit")
-
-        else
-            optiuni=givenString.split(";")
-            if optiuni[0]=="exit":
+        mesaj=input()
+        if mesaj=="help":
+            print("Add, id, pret, descriere, pret, locatie -> adauga obiect")
+            print("Delete, id -> sterge obiect")
+            print("Update, id -> modifica obiect")
+            print("Showall -> afiseaza toate obiectele")
+            print("Exit -> opreste programul")
+        else:
+            optiuni=mesaj.split(";")
+            if optiuni[0]=="Exit":
                 break
-            else
-                for optiune in optiuni:
-                    opti= optiune.split(",")
-                    if(opt[0]=="add"):
+            else:
+                for optiuni in optiuni:
+                    camp=optiune.split(",")
+                    if camp[0]=="Add":
                         try:
-                            lista= adaugaInventar(opti[1],opti[2],opti[3],float(opti[4],opti[5],lista))
+                            lista= addObject(camp[1], camp[2], camp[3], camp[4], camp[5], lista)
                         except ValueError as ve:
+                            print("Eroare : {}".format(ve))
+                    elif camp[0] == "Showall":
+                        ShowAll(lista)
+                    elif camp[0] == "Update":
+                        lista = modifyObject(camp[1], camp[2], camp[3], camp[4], camp[5], lista)
+                    elif camp[0] == "Delete":
+                        try:
+                            lista = deleteObject(camp[1], lista)
+                        except ValueError as ve1:
+                            print("Eroare : {}".format(ve1))
+                    else:
+                        print("Optiune gresita! Acceseaza comanda 'help'!")
+
+
+
 
