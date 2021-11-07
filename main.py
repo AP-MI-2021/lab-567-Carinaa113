14fe9f0a38524ddb9365718f87f5961d4f16e44c
Carinaa113 <carina.padurariu@stud.ubbcluj.ro>
2021-11-07 22:49:34 +0200

    tema lab 567-cu modificare in lista

--- main.py
+++ main.py
@@ -1,30 +1,24 @@
-from Logic.CRUD import adaugaCarte
 from UI.consola_noua import meniu
 from UI.console import runMenu
 from Tests.testAll import runAllTests
 
-def meniu_nou():
-    print("1.Rezolvare prima metoda al UI")
-    print("2.A doua metoda de rezolvare")
-    print("3.Iesire")
 
 def main():
     runAllTests()
+    lista=[]
 
     while True:
-        meniu_nou()
-        optiune=input("Dati optiunea: ")
-
-        if optiune=="1":
-            lista=[]
-            lista=adaugaCarte("1","Colt Alb", "aventura",15,"silver",lista)
-            lista=adaugaCarte("2","Scufita Rosie", "aventura",15,"silver",lista)
-            print(runMenu(lista))
-        elif optiune=="2":
-            print(meniu())
-        elif optiune=="3":
+        print("1.Consola clasica")
+        print("2.Consola noua")
+        print("3.Exit")
+        op=int(input("Alegeti consola: "))
+        if op==1:
+            lista=runMenu(lista)
+        elif op==2:
+            lista=meniu(lista)
+        elif op==3:
             break
         else:
-            print("Optiune gresita!Reincercati :")
+            print("Optiune invalida")
 
 main()
